aa8812201a6a5d13e5cdb6fb348b33e296b718ec
shipengqi <104604657@qq.com>
2018-06-05 21:46:45 +0800

    finish

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # eslint-config-sactive
 
 SActive's ESLint config, based on Standard.
+
+
+## Usage
+
+```bash
+npm install --save-dev eslint-config-sactive eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
+
+#or
+yarn add eslint-config-sactive eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
+```
+
+Add this to your `.eslintrc` file:
+```javascript
+{
+  "extends": "sactive"
+}
+```
--- index.js
+++ index.js
@@ -9,11 +9,15 @@ module.exports = {
   extends: "standard",
   rules: {
     "arrow-parens": [2, "as-needed"],
-    eqeqeq: 0,
+    eqeqeq: 2,
+    "indent": ["error", 2],
     "no-return-assign": 0, // fails for arrow functions
     "no-var": 2,
     semi: [2, "always"],
     "space-before-function-paren": [2, "never"],
+    "space-before-blocks": 2,
+    "space-before-function-paren": 2,
+    "comma-spacing": ["error", { "before": false, "after": true }],
     yoda: 0,
     "arrow-spacing": 2,
     "dot-location": [2, "property"],
