9fc26832c03b7588c2c8166130dd690f1f296365
Shi <peng-qi.shi@hpe.com>
2018-06-06 19:49:52 +0800

    string must be singlequote

--- index.js
+++ index.js
@@ -6,22 +6,22 @@
  */
 
 module.exports = {
-  extends: "standard",
+  extends: 'standard',
   rules: {
-    "arrow-parens": [2, "as-needed"],
+    'arrow-parens': [2, 'as-needed'],
     eqeqeq: 2,
-    "eol-last": 0,
-    quotes: ["error", "double", { "allowTemplateLiterals": true }],
-    "space-unary-ops": ["error", {"words": true, "nonwords": true}],
-    "no-return-assign": 0,
-    "no-var": 2,
-    semi: [2, "always"],
-    "space-before-function-paren": [2, "never"],
-    "space-before-blocks": 2,
-    "comma-spacing": ["error", { "before": false, "after": true }],
+    'eol-last': 0,
+    quotes: ['error', 'single', { 'allowTemplateLiterals': true }],
+    'space-unary-ops': ['error', {'words': true, 'nonwords': true}],
+    'no-return-assign': 0,
+    'no-var': 2,
+    semi: [2, 'always'],
+    'space-before-function-paren': [2, 'never'],
+    'space-before-blocks': 2,
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
     yoda: 0,
-    "arrow-spacing": 2,
-    "dot-location": [2, "property"],
-    "prefer-arrow-callback": 2
+    'arrow-spacing': 2,
+    'dot-location': [2, 'property'],
+    'prefer-arrow-callback': 2
   }
 };
