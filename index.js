d6bdacbfc48827d9767ae712a90e44fa70bc63c6
shipengqi <104604657@qq.com>
2018-06-10 20:41:31 +0800

    add some rules

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ module.exports = {
     eqeqeq: 2,
     'eol-last': 0,
     quotes: ['error', 'single', { 'allowTemplateLiterals': true }],
-    'space-unary-ops': ['error', {'words': true, 'nonwords': true}],
     'no-return-assign': 0,
     'no-var': 2,
     semi: [2, 'always'],
-    'space-before-function-paren': [2, 'never'],
     'space-before-blocks': 2,
     'comma-spacing': ['error', { 'before': false, 'after': true }],
     yoda: 0,
     'arrow-spacing': 2,
     'dot-location': [2, 'property'],
-    'prefer-arrow-callback': 2
+    'prefer-arrow-callback': 2,
+    'space-unary-ops':
+      ['error', {
+        'words': true,
+        'nonwords': true,
+        'overrides': {
+          '!': false
+        }
+      }],
+    'no-useless-call': 0,
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never',
+      'named': 'never',
+      'asyncArrow': 'ignore'
+    }],
+    'new-cap': ['error', { 'newIsCap': false }]
+
   }
 };
